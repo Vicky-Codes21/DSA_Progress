3c214ca506b3de07c0b268761571bc5b54e8bd68
Vivek <vky45615@gmail.com>
2026-09-15 22:27:25 +0530

    Solved Lower&Upper Bound Problems

---
+++ Binary_Search/03_Upper_Bound.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lowerBound(vector<int>& nums, int x) {
+        int n = nums.size();
+        int st = 0;
+        int end = n - 1;
+        int ans = n;
+
+        while(st <= end) {
+            int mid = st + (end - st) / 2;
+
+            if(nums[mid] > x) {
+                ans = mid;      // possible answer
+                end = mid - 1;  // search for an earlier answer
+            }
+            else {
+                st = mid + 1;   // search right
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    vector<int> ans = {1,2,3,4,5,6,7,8};
+    int x = 6;
+
+    Solution obj;
+    int value = obj.lowerBound(ans, x);
+
+    cout << "Index is : " << value;
+
+    return 0;
+}
